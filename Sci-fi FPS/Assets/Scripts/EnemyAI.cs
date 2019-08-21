49e36fa73433e4e853fc43506778e4c0e84eaa41
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-21 22:29:57 +0200

    Started writing some simple functions for enemy ai

--- Sci-fi FPS/Assets/Scripts/EnemyAI.cs
+++ Sci-fi FPS/Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,7 +8,8 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float chaseRange = 5f;
-
+    [SerializeField] private float attackRange = 1f;
+    private bool isProvoked = false;
     private NavMeshAgent navMeshAgent;
 
     private float distanceToTarget=Mathf.Infinity;
@@ -19,9 +21,41 @@ public class EnemyAI : MonoBehaviour
     {
         
         distanceToTarget=Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Git attacked ya biscuit!");
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color=Color.red;
+        Gizmos.DrawWireSphere(transform.position,chaseRange);
     }
 }
