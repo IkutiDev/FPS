085bbd0a826b2874ece3916759adbe6e63693ac4
Ikuti <m.j.olkiewicz@gmail.com>
2019-08-30 20:43:23 +0200

    Finished muzzle for gun and other things

--- Sci-fi FPS/Assets/Scripts/Weapon.cs
+++ Sci-fi FPS/Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Camera FPCamera;
     [SerializeField] private float range = 100f;
     [SerializeField] private float damage = 10f;
+    [SerializeField] private ParticleSystem muzzleFlash;
 
     private void Update()
     {
@@ -19,16 +20,25 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if (Physics.Raycast(FPCamera.transform.position,FPCamera.transform.forward,out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name + " was shot");
             // TODO: add some hit effect for visual players
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if (target != null)
-            {
-                target.TakeDamage(damage);
-            }
+            if (target == null) return;
+            target.TakeDamage(damage);
         }
         else
         {
