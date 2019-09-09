336223e0e50e107fc1d817e48322aa9c88ba8796
Ikuti <m.j.olkiewicz@gmail.com>
2019-09-10 00:13:05 +0200

    Added scene loading, removed unused assets

--- Sci-fi FPS/Assets/Scripts/SceneLoader.cs
+++ Sci-fi FPS/Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void RestartGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
